11c39be496d61c1986b14b1a14922ef6357b58a1
lvyang <tylvyang@qq.com>
2020-05-01 16:05:56 +0800

    add go-04

--- go_learning/src/ch1/main/hello_world.go
+++ go_learning/src/ch1/main/hello_world.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+// func main() {
+// 	fmt.Println("Hello World")
+// }
+//
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) > 1 {
+		fmt.Println("Hello World", os.Args[1])
+	}
 }
